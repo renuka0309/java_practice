616789f6bc8f2e1a18c1ff1cd065cf53f518d24e
renuka0309 <renukaullegaddi03@gmail.com>
2026-04-13 22:35:16 +0530

    Add files via upload

---
+++ A_divisibility.java
@@ -0,0 +1,11 @@
+class A_divisibility{
+ public static void main(String[] args){
+  int num = 15;
+
+  if(num % 3 == 0 && num % 5 == 0){
+   System.out.println("Number is divisible by 3 and 5");
+  } else{
+   System.out.println("Number is not divisible by 3 and 5");
+  }
+ }
+}
