5982da60280b379f79c009f58e08a8e776a7deb3
renuka0309 <renukaullegaddi03@gmail.com>
2026-04-21 19:33:55 +0530

    Add files via upload

---
+++ Matrimonial.java
@@ -0,0 +1,16 @@
+class Matrimonial{
+    public static void main(String[] args) {
+        int age=21;
+        String gender="Female";
+
+        if(age>=21){
+          if(gender=="female"){
+            System.out.println("signed up");
+          }
+        }
+        else{
+            System.out.println("invalid");
+        }
+    }
+
+}
